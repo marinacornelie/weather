89b63a252915bea5f723c5d55bd30cc8b8d9d928
Lolotte Schimmel <lmcschimmel@gmail.com>
2021-04-08 15:22:10 +0200

    add weather forecast, reset state and show error function

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import SelectUnits from './SelectUnits'
 const apiKey = process.env.REACT_APP_WEATHER_API_KEY
 
 class App extends Component {
-
   state = {
     city: '',
     units: 'metric',
     weatherInfo: '',
+    forecastInfo: [ ],
     errorMessage: ''
   }
 
@@ -17,11 +17,28 @@ class App extends Component {
     this.setState({units: unitsNew})
   }
 
-  currentWeather = () => {
-    this.setState({
+  showError = (error) => {
+    if (error.response.status === 404) {
+      this.setState({
+        errorMessage: 'Sorry, unknown city!',
+      })
+    } else {
+      this.setState({
+        errorMessage:'Did you forget to fill in something?',
+      })
+    }
+  }
+
+  resetState = () => {
+    this.setState ({
       weatherInfo: '',
+      forecastInfo: [ ],
       errorMessage: ''
     })
+  }
+
+  currentWeather = () => {
+   this.resetState();
     axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + this.state.city + '&units=' + this.state.units + '&appid=' + apiKey)
     .then(response => {
       this.setState({
@@ -29,31 +46,41 @@ class App extends Component {
       }) 
     })
     .catch((error) => {
-      if (error.response.status === 404) {
-        this.setState({
-          errorMessage: 'Sorry, unknown city!',
-        })
-      } else {
-        this.setState({
-          errorMessage:'Did you forget to fill in something?',
-        })
-      }
+      this.showError(error);
+    })
+  } 
+
+  forecastWeather = () => {
+    this.resetState();
+    axios.get('https://api.openweathermap.org/data/2.5/forecast?q=' + this.state.city + '&units=' + this.state.units + '&appid=' + apiKey)
+    .then(response => {
+      this.setState({
+        forecastInfo: response.data.list
+      }) 
+    })
+    .catch((error) => {
+      this.showError(error);
     })
   }
-
+  
   render() {   
     return (
       <div>
         <div>
           <span>Show me the weather in</span>
-          <input onChange={event => this.setState({city: event.target.value})}/>
+          <input placeholder="city name" onChange={event => this.setState({city: event.target.value})}/>
           <SelectUnits changeUnits={this.changeUnits}/>
-          <button onClick={this.currentWeather}>Go!</button>
+          <button onClick={this.currentWeather}>Show me the current weather!</button>
+          <button onClick={this.forecastWeather}>Show me the 5 day weather forecast!</button>
         </div>
-        <div>
-          <span>{this.state.weatherInfo || this.state.errorMessage }</span>
-        </div>
-       </div>
+        <div>{this.state.weatherInfo}</div>
+        <ul>
+          {this.state.forecastInfo.map((item) => ( 
+            <li key={item.dt}>{item.dt_txt}, {item.main.temp}, {item.weather[0].main}</li>
+          ))}       
+        </ul>
+        <div>{this.state.errorMessage}</div>
+      </div>
     )
   }
 }
